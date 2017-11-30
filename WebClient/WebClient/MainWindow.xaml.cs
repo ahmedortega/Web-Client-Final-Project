2095fb52cdc551a252ce954bea904f0a1b482f60
DESKTOP-4KP52D6\Ahmed Ortega <ahmedsniper2020@outlook.com>
2017-11-30 21:55:08 +0200

    bb

--- WebClient/WebClient/MainWindow.xaml.cs
+++ WebClient/WebClient/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,19 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http.Headers;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Fname { get; set; }
+    public string Lname { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public int UserType { get; set; }
+}
 
 namespace WebClient
 {
@@ -23,6 +37,89 @@ namespace WebClient
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Text_Username(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void Text_Password(Object sender, RoutedEventArgs args)
+        {
+        }
+        private void Button_Login(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost/JournalProjectWebApp/");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var emp = new Employee();
+            emp.Username = UsernameLabel.Text.Trim();
+            emp.Password = PasswordLabel.Password.Trim();
+            emp.UserType = ComboUserType.SelectedIndex + 1;
+            if (emp.UserType == 1)
+            {
+                var response = client.PostAsJsonAsync("Login/searchV/", emp).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("U Logged in successfuly");
+                }
+                else
+                {
+                    MessageBox.Show("the Username or Password is not Correct");
+                }
+            }
+            else
+            {
+                MessageBox.Show("U Failed");
+            }
+            /*var emp = new Employee();
+            emp.Username = UsernameLabel.Text.Trim();
+            emp.Password = PasswordLabel.Password.Trim();
+            emp.UserType = ComboUserType.SelectedIndex + 1;
+            var response = client.PostAsJsonAsync("Login/search/", emp).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("U Logged in successfuly");
+            }
+            else
+            {
+                MessageBox.Show(response.StatusCode + " With this Message " + response.ReasonPhrase);
+            }
+            */
+        }
+
+        /*public int ComboChecker(string combovalue)
+        {
+            int result = 0;
+            switch(combovalue)
+            {
+                case "0":
+                    result = 1;
+                    break;
+                case "1":
+                    result = 2;
+                    break;
+                case "2":
+                    result = 3;
+                    break;
+            }
+            return result;
+            switch (result)
+            {
+                case 1:
+                    MessageBox.Show("its 1");
+                    break;
+                case 2:
+                    MessageBox.Show("its 2");
+                    break;
+                case 3:
+                    MessageBox.Show("its 3");
+                    break;
+            }
+        }
+        */
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
